c73c84ed90130b5a56437a46b4a46478b47b3a1d
khoa190299 <dangkhoa.tech@smit.vn>
2023-07-13 15:10:58 +0700

    init

---
+++ src/views/Blogs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <el-header
+      ><el-container><Header /></el-container
+    ></el-header>
+    <el-main>Main</el-main>
+    <el-footer><Footer /></el-footer>
+  </div>
+</template>
+
+<script>
+import Header from "../layout/Header.vue";
+import Footer from "../layout/Footer.vue";
+export default {
+  name: "blogs-view",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+<style lang="scss"></style>
